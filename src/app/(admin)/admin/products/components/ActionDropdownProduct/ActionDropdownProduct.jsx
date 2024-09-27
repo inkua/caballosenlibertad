0ef1bc9614f981359f8630d019ffd821eeee4a89
manuel14mds <manuel14mds@gmail.com>
2024-09-27 16:07:15 -0500

    feature: get product bay id finish

--- src/app/(admin)/admin/products/components/ActionDropdownProduct/ActionDropdownProduct.jsx
+++ src/app/(admin)/admin/products/components/ActionDropdownProduct/ActionDropdownProduct.jsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react"
 import BtnEditProduct from "../BtnEditProduct/BtnEditProduct"
+import Link from "next/link"
 
 function ActionDropdownProduct({ data }) {
     const [toggle, SetToggle] = useState(false)
@@ -20,7 +21,7 @@ function ActionDropdownProduct({ data }) {
                 <div className="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabIndex="-1">
                     <div className="py-1" role="none">
                         <BtnEditProduct data={data} />
-                        <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:underline" role="menuitem" tabIndex="-1" id="menu-item-1">Detail</a>
+                        <Link href={`/admin/products/${data.id}`} className="block px-4 py-2 text-sm text-gray-700 hover:underline" role="menuitem" tabIndex="-1" id="menu-item-1">Detail</Link>
                         <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:underline" role="menuitem" tabIndex="-1" id="menu-item-2">Settings</a>
                     </div>
                     <div className="py-1" role="none">
