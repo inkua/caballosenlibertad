e7693b9b3d98e1ad41c2b500c9c3b176a42435f5
mauriGaray <garaycarlosmauricio17@gmail.com>
2024-10-10 20:00:59 -0300

    image support v0

--- src/app/(admin)/admin/products/components/FormProduct/FormProduct.jsx
+++ src/app/(admin)/admin/products/components/FormProduct/FormProduct.jsx
@@ -12,6 +12,7 @@ function FormProduct({ isOpen, setIsOpen, saveData, data, add = true }) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
     const newData = {
       name,
       price,
@@ -191,6 +192,7 @@ function FormProduct({ isOpen, setIsOpen, saveData, data, add = true }) {
                 Cancelar
               </button>
               <button
+                onClick={() => alert("Funcionalidad no disponible")}
                 type="submit"
                 className="px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
                 {add ? "Guardar Producto" : "Editar Producto"}
