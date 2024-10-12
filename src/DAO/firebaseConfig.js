9b288a8827fcb3545b86fc24074315cec9047055
gevega <gevegaecheverria@gmail.com>
2024-10-12 19:07:55 -0300

    Firebase config

--- src/DAO/firebaseConfig.js
+++ src/DAO/firebaseConfig.js
@@ -4,11 +4,11 @@ import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.FIREBASE,
-  authDomain: "test-caballos-en-libertad.firebaseapp.com",
-  projectId: "test-caballos-en-libertad",
-  storageBucket: "test-caballos-en-libertad.appspot.com",
-  messagingSenderId: "1091894138986",
-  appId: "1:1091894138986:web:0eb98130c65889a4e3d213"
+  authDomain: process.env.AUTHDOMAIN,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID,
+  appId: process.env.APPID
 };
 
 const app = initializeApp(firebaseConfig);
