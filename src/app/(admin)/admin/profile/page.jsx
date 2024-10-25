759a25ec6834787f4929e4578fdae70921ff6972
githubfacu <elorzfacundo@gmail.com>
2024-10-24 21:47:15 -0300

    toast function, toastContainer

--- src/app/(admin)/admin/profile/page.jsx
+++ src/app/(admin)/admin/profile/page.jsx
@@ -1,3 +1,11 @@
+'use client'
+import { useToast } from "@/utils/toast"
+
+const { showToast } = useToast()
+
+function imgClick(){
+    showToast({type:'warning', message:'Margot Foster'})
+}
 
 function ProductDetail() {
     return (
@@ -5,7 +13,7 @@ function ProductDetail() {
             <div className="px-4 sm:px-0">
                 <h3 className="text-base font-semibold leading-7 text-gray-900">Applicant Information</h3>
                 <div class="flex items-center gap-x-6">
-                    <img className="h-16 w-16 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+                    <img onClick={() => imgClick()} className="h-16 w-16 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
                         <div>
                             <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900">Leslie Alexander</h3>
                             <p className="text-sm font-semibold leading-6 text-indigo-600">Co-Founder / CEO</p>
