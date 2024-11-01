8663638a1ebc1cd6879351db742265a90ef50808
Paola Andrea Moreno <136043704+IngSw-Moreno7@users.noreply.github.com>
2024-11-01 17:25:03 -0500

    Update page code and create assets folder with images for the about us page

--- src/app/(main)/sobrenosotros/page.jsx
+++ src/app/(main)/sobrenosotros/page.jsx
@@ -1,64 +1,54 @@
 import React from 'react';
 
 const SobreNosotros = () => {
-  return (
-    <div style={{ backgroundColor: '#FFFFFF', padding: '20px', fontFamily: 'Poppins' }}>
-      <h1 style={{ fontWeight: 275, fontSize: '36px', lineHeight: '46.8px', color: '#03151F' }}>
-        Nuestra Misión
-      </h1>
-      <h2 style={{ fontWeight: 800, fontSize: '60px', lineHeight: '72px', color: '#00638C' }}>
-        Rescatamos, rehabilitamos y reubicamos caballos
-      </h2>
+    return (
+        <section className="bg-white">
+            <img className="hidden md:block object-cover w-[1440px] h-[670px] t-[90px]" src="https://images.unsplash.com/photo-1556761175-5973dc0f32e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80" alt="Imagen circular y responsiva" />
+            <div className="container px-6 py-16 mx-auto text-center">
+                <span className="text-2xl font-semibold text-[#03151F] lg:text-xl">Nuestra Misión</span>
+                <h1 className="font-bold text-4xl text-[#00638C] mt-4">Rescatamos, rehabilitamos y reubicamos caballos</h1>
 
-      {/* Información sobre Caballos */}
-      <div style={{ display: 'flex', alignItems: 'flex-start', marginTop: '20px' }}>
-        <img src="ruta-a-imagen-caballos.jpg" alt="Caballos" style={{ borderRadius: '50%', width: '150px', height: '150px', marginRight: '20px' }} />
-        <div style={{ backgroundColor: '#00638C', color: '#FFFFFF', padding: '10px', borderRadius: '5px', width: '100%' }}>
-          <p style={{ fontSize: '12px', fontFamily: 'Poppins' }}>
-            Caballos en libertad nace hace más de 10 años como una Asociación civil sin fines de lucro encargada de la protección, cuidado y rescate de equinos, siendo una de las más importantes de la Provincia de Buenos Aires.
-          </p>
-        </div>
-      </div>
+                <article className="flex items-start mt-8">
+                    <img className="object-cover rounded-full w-24 h-24 sm:w-32 sm:h-32 md:w-40 md:h-40 lg:w-48 lg:h-48" src="https://images.unsplash.com/photo-1556761175-5973dc0f32e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80" alt="Imagen circular y responsiva" />
+                    <div className="bg-[#00638C] text-white p-4 rounded-lg flex-1">
+                        <p className="text-sm">
+                        <span className="font-bold">Caballos en libertad</span> nace hace más de 10 años como una Asociación civil sin fines de lucro encargada de la protección, cuidado y rescate de equinos, siendo una de las más importantes de la Provincia de Buenos Aires.
+                        </p>
+                    </div>
+                </article>
 
-      {/* Información sobre la ubicación */}
-      <div style={{ display: 'flex', alignItems: 'flex-start', marginTop: '20px' }}>
-        <img src="ruta-a-imagen-mapa.jpg" alt="Mapa" style={{ borderRadius: '50%', width: '150px', height: '150px', marginRight: '20px' }} />
-        <div style={{ backgroundColor: '#00638C', color: '#FFFFFF', padding: '10px', borderRadius: '5px', width: '100%' }}>
-          <p style={{ fontSize: '12px', fontFamily: 'Poppins' }}>
-            Nos ubicamos en Zona Sur, ciudad de La Plata, Provincia de Buenos Aires (uno de los principales epicentros de maltrato y abandono equino).
-          </p>
-        </div>
-      </div>
+                <article className="flex items-start mt-8">
+                    <img className="object-cover rounded-full w-24 h-24 sm:w-32 sm:h-32 md:w-40 md:h-40 lg:w-48 lg:h-48" src="https://images.unsplash.com/photo-1556761175-5973dc0f32e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80" alt="Imagen circular y responsiva" />
+                    <div className="bg-[#00638C] text-white p-4 rounded-lg flex-1">
+                        <p className="text-sm">
+                            Nos ubicamos en Zona Sur, ciudad de La Plata, Provincia de Buenos Aires (uno de los principales epicentros de maltrato y abandono equino).
+                        </p>
+                    </div>
+                </article>
 
-      {/* Información sobre el veterinario */}
-      <div style={{ display: 'flex', alignItems: 'flex-start', marginTop: '20px' }}>
-        <img src="ruta-a-imagen-veterinario.jpg" alt="Veterinario" style={{ borderRadius: '50%', width: '150px', height: '150px', marginRight: '20px' }} />
-        <div style={{ backgroundColor: '#00638C', color: '#FFFFFF', padding: '10px', borderRadius: '5px', width: '100%' }}>
-          <p style={{ fontSize: '12px', fontFamily: 'Poppins' }}>
-            Nuestra ONG cuenta con el apoyo de una eminencia en veterinaria, el doctor Juan Oliden quien, junto a nuestros voluntarios, y gracias a la colaboración ciudadana, brinda a nuestros rescatados la atención, el amor y la medicación que en algunos casos corresponde.
-          </p>
-        </div>
-      </div>
+                <article className="flex items-start mt-8">
+                    <img className="object-cover rounded-full w-24 h-24 sm:w-32 sm:h-32 md:w-40 md:h-40 lg:w-48 lg:h-48" src="https://images.unsplash.com/photo-1556761175-5973dc0f32e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80" alt="Imagen circular y responsiva" />
+                    <div className="bg-[#00638C] text-white p-4 rounded-lg flex-1">
+                        <p className="text-sm">
+                            Nuestra ONG cuenta con el apoyo de una eminencia en veterinaria, el doctor Juan Oliden quien, junto a nuestros voluntarios, y gracias a la colaboración ciudadana, brinda a nuestros rescatados la atención, el amor y la medicación que en algunos casos corresponde.
+                        </p>
+                    </div>
+                </article>
 
-      {/* Información sobre la esperanza */}
-      <div style={{ display: 'flex', alignItems: 'flex-start', marginTop: '20px' }}>
-        <img src="ruta-a-imagen-caballos.jpg" alt="Caballos" style={{ borderRadius: '50%', width: '150px', height: '150px', marginRight: '20px' }} />
-        <div style={{ backgroundColor: '#00638C', color: '#FFFFFF', padding: '10px', borderRadius: '5px', width: '100%' }}>
-          <p style={{ fontSize: '12px', fontFamily: 'Poppins' }}>
-            Con la esperanza de un día verlos libres, felices y sin ser utilizados como objetos para la tracción a sangre, esperamos poder construir un futuro libre de crueldad animal, en donde cada persona aporte su granito de arena y participe en casos de indiferencia o maltrato a cualquier ser vivo.
-          </p>
-        </div>
-      </div>
+                <article className="flex items-start mt-8">
+                    <img className="object-cover rounded-full w-24 h-24 sm:w-32 sm:h-32 md:w-40 md:h-40 lg:w-48 lg:h-48" src="https://images.unsplash.com/photo-1556761175-5973dc0f32e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80" alt="Imagen circular y responsiva" />
+                    <div className="bg-[#00638C] text-white p-4 rounded-lg flex-1">
+                        <p className="text-sm">
+                            Con la esperanza de un día verlos libres, felices y sin ser utilizados como objetos para la tracción a sangre, esperamos poder construir un futuro libre de crueldad animal, en donde cada persona aporte su granito de arena y participe en casos de indiferencia o maltrato a cualquier ser vivo.
+                        </p>
+                    </div>
+                </article>
+            </div>
 
-      {/* Mensaje final */}
-      <h2 style={{ fontWeight: 800, fontSize: '50px', lineHeight: '75px', color: '#00638C', marginTop: '40px' }}>
-        ¡Apostamos a un futuro con derechos e igualdad para todos los animales!
-      </h2>
-
-      {/* Imagen final */}
-      <img src="ruta-a-imagen-final.jpg" alt="Imagen final" style={{ width: '100%', marginTop: '20px' }} />
-    </div>
-  );
+                <h2 className="font-bold text-center text-[20px] md:text-[30px] text-[#00638C] mt-10">¡Apostamos a un futuro con derechos e igualdad para todos los animales!</h2>
+                <img className="hidden md:block object-cover w-full h-screen" src="https://images.unsplash.com/photo-1556761175-5973dc0f32e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80" alt="Imagen circular y responsiva" />
+        </section>
+    );
 };
 
 export default SobreNosotros;
