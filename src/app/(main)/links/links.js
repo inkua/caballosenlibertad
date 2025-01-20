4ff622a69d97b4cb4974fa39e6bb8b89b3805fb3
Paola Andrea Moreno <136043704+IngSw-Moreno7@users.noreply.github.com>
2025-01-20 18:12:07 -0500

    Updated Links.js to improve links to main sections

--- src/app/(main)/links/links.js
+++ src/app/(main)/links/links.js
@@ -1,11 +1,14 @@
 export const featuresLinks = [
-    { textContent: 'Sobre Nosotros', href: '/nosotros'},
-    { textContent: 'Rescates', children: [
-        { textContent: 'Historias de éxito', href: '/'},
-        { textContent: 'En adopción', href: '/adopciones'},
-    ]},
-    { textContent: 'Voluntariado', href: '/'},
-    { textContent: 'Novedades', href: '/'},
-    { textContent: 'Denuncias', href: '/'},
-    { textContent: 'Contacto', href: '/'},
-]
+  { textContent: 'Sobre Nosotros', href: '/nosotros' },
+  {
+    textContent: 'Caballos',
+    children: [
+      { textContent: 'Historias de éxito', href: '/' },
+      { textContent: 'En adopción', href: '/' },
+    ],
+  },
+  { textContent: 'Voluntariado', href: '/' },
+  { textContent: 'Novedades', href: '/' },
+  { textContent: 'Denuncias', href: '/' },
+  { textContent: 'Contacto', href: '/' },
+];
