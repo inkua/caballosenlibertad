f169277a7ef010f7681f58777a5cbef4c4d3ee69
facu_ <110848864+githubfacu@users.noreply.github.com>
2025-01-21 19:19:05 -0300

    donation form layout (#44)

--- src/app/(main)/components/donar/HelpDonation.jsx
+++ src/app/(main)/components/donar/HelpDonation.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { HelpItem } from './HelpItem'
-import { DonationForm } from './DonationForm'
+import { DonationCard } from './DonationCard'
 
 export const HelpDonation = () => {
   return (
     <div className='grid lg:grid-cols-2 gap-12 lg:gap-16'>
         <div>
+            <h2 className='mb-14 md:mb-20 lg:mb-24 text-start text-[28px] text-primary font-bold leading-140 md:text-[36px] lg:text-[42px] xl:text-[50px] lg:font-extrabold'>¿Cómo ayudar?</h2>
             <HelpItem priority={true} styleTypeImage={'/assets/donar/Group1.svg'} text={`Donación \nmonetaria`} image={'/assets/donar/Aporte-monetario.svg'}/>
-            <div className='flex flex-col bg-primary2 text-[16px] md:text-[20px] lg:text-[24px] p-8 rounded-2xl mt-16'>
+            <div className='flex flex-col bg-primary2 text-[16px] md:text-[20px] lg:text-[22px] xl:text-[24px] p-8 rounded-2xl mt-16'>
                 <p className='font-light'>
                     Si eres de Argentina podés transferir a nuestra cuenta:
                 </p>
@@ -19,7 +20,7 @@ export const HelpDonation = () => {
                 <span><strong>NOMBRE:</strong> CABALLOS EN LIBERTAD ASOC CIV</span>
             </div>
         </div>
-        <DonationForm />
+        <DonationCard />
     </div>
   )
 }
