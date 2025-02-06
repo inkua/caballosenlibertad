092d077e956a240a90b91d99435a7077d0da1d6c
gevega <gevegaecheverria@gmail.com>
2025-02-06 14:50:56 -0300

    feat: add slider component

---
+++ src/app/(main)/components/slider/Slider.jsx
@@ -0,0 +1,40 @@
+"use client"
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
+import "swiper/css/navigation";
+import "swiper/css/bundle";
+import "swiper/css";
+import "./custom-swiper-bullet.css";
+
+import Image from "next/image";
+
+function Slider({ items, type = 0, def, lg, md, nav = true, loop = false }) {
+    const customBar = type == 0 ? import("./custom-bar-0.css") : import("./custom-bar-1.css");
+
+    return (
+        <Swiper
+            modules={[Navigation, Pagination]}
+            navigation={nav}
+            pagination={type == 0 ? false : { clickable: true }}
+            slidesPerView={def}
+            breakpoints={{ 1024: { slidesPerView: lg }, 768: { slidesPerView: md } }}
+            spaceBetween={type == 0 ? 10 : 100}
+            loop={loop}
+            className={`!relative w-full lg:w-[75%] cursor-grab h-auto ${type == 0 ? '!pb-0 !px-4' : '!pb-12 !px-16'}`} >
+            {items.map((item) => (
+                <SwiperSlide key={item.id} className={`!h-auto overflow-hidden ${type == 0 ? 'rounded-none' + ' aspect-[3/2]' : 'rounded-2xl' + ' aspect-square'}`}>
+                    <Image
+                        src={item.data}
+                        alt={item.alt}
+                        width={700}
+                        height={700}
+                        className="absolute w-full h-full object-cover"
+                        loading="eager"
+                    />
+                </SwiperSlide>
+            ))}
+        </Swiper>
+    );
+}
+
+export default Slider;
