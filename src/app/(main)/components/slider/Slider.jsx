cac99fce46b06d591f657e602344f926727164da
gevega <gevegaecheverria@gmail.com>
2025-02-06 19:46:43 -0300

    feat: Add Link slider

--- src/app/(main)/components/slider/Slider.jsx
+++ src/app/(main)/components/slider/Slider.jsx
@@ -7,6 +7,7 @@ import "swiper/css";
 import "./custom-swiper-bullet.css";
 
 import Image from "next/image";
+import Link from "next/link";
 
 function Slider({ items, type = 0, def, lg, md, nav = true, loop = false }) {
     const customBar = type == 0 ? import("./custom-bar-0.css") : import("./custom-bar-1.css");
@@ -23,14 +24,28 @@ function Slider({ items, type = 0, def, lg, md, nav = true, loop = false }) {
             className={`!relative w-full lg:w-[75%] cursor-grab h-auto ${type == 0 ? '!pb-0 !px-4' : '!pb-12 !px-16'}`} >
             {items.map((item) => (
                 <SwiperSlide key={item.id} className={`!h-auto overflow-hidden ${type == 0 ? 'rounded-none' + ' aspect-[3/2]' : 'rounded-2xl' + ' aspect-square'}`}>
-                    <Image
-                        src={item.data}
-                        alt={item.alt}
-                        width={700}
-                        height={700}
-                        className="absolute w-full h-full object-cover"
-                        loading="eager"
-                    />
+                    {item.url ?
+                        <Link href={item.url} target="_blank" rel="noopener noreferrer">
+                            <Image
+                                src={item.data}
+                                alt={item.alt}
+                                width={700}
+                                height={700}
+                                className="absolute w-full h-full object-cover"
+                                loading="eager"
+                            />
+                        </Link>
+                        :
+                        <Image
+                            src={item.data}
+                            alt={item.alt}
+                            width={700}
+                            height={700}
+                            className="absolute w-full h-full object-cover"
+                            loading="eager"
+                        />
+                    }
+
                 </SwiperSlide>
             ))}
         </Swiper>
