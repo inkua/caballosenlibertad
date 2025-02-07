fb05bed51dec94f6142a703c1e2a9a9d4fa11709
gevega <gevegaecheverria@gmail.com>
2025-02-07 19:29:33 -0300

    feat: Add Denuncias page

--- src/app/(main)/denuncias/page.jsx
+++ src/app/(main)/denuncias/page.jsx
@@ -1,24 +1,112 @@
-import React from 'react'
+import { TitleSection } from "../components/TitleSection"
+import Image from "next/image";
 
-export default function page() {
+const items = [
+    {
+        data: '/assets/caballos_atardecer.jpg',
+        alt: '',
+        url: '',
+    },
+    {
+        data: '/assets/caballos_rescatados.jpg',
+        alt: '',
+        url: '',
+    },
+    {
+        data: '/assets/doctor_veterinario.jpg',
+        alt: '',
+        url: '',
+    },
+    {
+        data: '/assets/equipo_ong_caballos.jpg',
+        alt: '',
+        url: '',
+    },
+]
+
+function Denuncias() {
     return (
-        <main className='pt-[70px] lg:pt-[90px] bg-blue-950'>
-            <header className='mx-mobile md:mx-tablet lg:mx-desktop grid grid-cols-4 md:grid-cols-12 bg-green-200 xl:mx-auto max-w-content'>
-                <h1 className='col-span-full lg:col-start-2 lg:col-end-12'>Denuncias</h1>
-                <p className='col-span-full lg:col-start-2 lg:col-end-8'>Entérate de como actuar si ves una situación de maltrato animal.</p>
-            </header>
+        <main className="mx-mobile my-28 sm:m-tablet  lg:m-desktop">
+            <section className="m-auto max-w-content">
+                <TitleSection
+                    titleTextContent="Denuncias"
+                    spanTextContent="Enterate de como actuar si ves una situación de maltrato animal."
+                />
 
-            <div className='mx-mobile md:mx-tablet lg:mx-desktop grid grid-cols-4 md:grid-cols-12 bg-green-300 xl:mx-auto max-w-content'>
-                <span className='col-span-full lg:col-start-2 lg:col-end-9 bg-red-100'>¿Cómo reportar una situación de maltrato?</span>
-            </div>
+                <div>
+                    <h2 className="text-primary text-h2 font-bold leading-120 mt-12 mb-12">¿Cómo reportar situación de maltrato?</h2>
+                    <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-32 lg:gap-x-72 gap-y-24 md:gap-y-24 lg:gap-y-32 items-start">
+                        <div className="flex flex-col w-full justify-center items-center gap-8">
+                            <div className="flex justify-center items-center w-[60%] aspect-square bg-primary2 rounded-full overflow-hidden">
+                                <Image
+                                    src={'/assets/denuncias/denuncia_01.png'}
+                                    alt={'No disponible'}
+                                    width={700}
+                                    height={700}
+                                    className="object-cover w-[70%] h-auto"
+                                    loading="eager"
+                                />
+                            </div>
+                            <h2 className="text-h2 text-primary font-bold text-center">Denunciar</h2>
+                            <p className="text-p2 text-center">
+                                Podés llamar al <span className="font-bold">911</span> o dirigirte a la <span className="font-bold">Fiscalía más cercana</span> de la zona en donde ocurre el delito.
+                            </p>
+                        </div>
 
-            <div className='mx-mobile md:mx-tablet lg:mx-desktop grid grid-cols-4 md:grid-cols-12 bg-blue-300 xl:mx-auto max-w-content'>
-                <div className='col-span-full lg:col-start-2 lg:col-end-5 bg-slate-400'>card 1</div>
-                <div className='col-span-full lg:col-start-8 lg:col-end-12 bg-slate-400'>card 2</div>
-                <div className='col-span-full lg:col-start-2 lg:col-end-5 bg-slate-400'>card 3</div>
-                <div className='col-span-full lg:col-start-8 lg:col-end-12 bg-slate-400'>card 4</div>
-            </div>
+                        <div className="flex flex-col w-full justify-center items-center gap-8">
+                        <div className="flex justify-center items-center w-[60%] aspect-square bg-primary2 rounded-full overflow-hidden">
+                        <Image
+                                    src={'/assets/denuncias/denuncia_02.png'}
+                                    alt={'No disponible'}
+                                    width={700}
+                                    height={700}
+                                    className="object-cover w-[70%] h-auto"
+                                    loading="eager"
+                                />
+                            </div>
+                            <h2 className="text-h2 text-primary font-bold text-center">Si no quieren tomar la denuncia</h2>
+                            <p className="text-p2 text-center">
+                                Dirigete a la Fiscalía para realizar la misma. Tambien <span className="font-bold">se puede denunciar a los oficiales con nombre y apellido</span> que no quisieron tomarla o, en su defecto, el número del operador del <span className="font-bold">911</span>.
+                            </p>
+                        </div>
 
+                        <div className="flex flex-col w-full justify-center items-center gap-8">
+                        <div className="flex justify-center items-center w-[60%] aspect-square bg-primary2 rounded-full overflow-hidden">
+                        <Image
+                                    src={'/assets/denuncias/denuncia_03.png'}
+                                    alt={'No disponible'}
+                                    width={700}
+                                    height={700}
+                                    className="object-cover w-[70%] h-auto"
+                                    loading="eager"
+                                />
+                            </div>
+                            <h2 className="text-h2 text-primary font-bold text-center">Dónde es un delito</h2>
+                            <p className="text-p2 text-center">
+                                La tracción a sangre a tiro animal <span className="font-bold">sólo esta prohibida en CABA.</span> Los Municipios que tienen ordenanzas de prohibición son los de <span className="font-bold">La Plata, San Martín y Quilmes.</span>
+                            </p>
+                        </div>
+
+                        <div className="flex flex-col w-full justify-center items-center gap-8">
+                        <div className="flex justify-center items-center w-[60%] aspect-square bg-primary2 rounded-full overflow-hidden">
+                        <Image
+                                    src={'/assets/denuncias/denuncia_04.png'}
+                                    alt={'No disponible'}
+                                    width={700}
+                                    height={700}
+                                    className="object-cover w-[70%] h-auto"
+                                    loading="eager"
+                                />
+                            </div>
+                            <h2 className="text-h2 text-primary font-bold text-center">Cuándo puedo denunciar</h2>
+                            <p className="text-p2 text-center">
+                                Todas las leyes se aplican cuando el delito fue cometido. No se puede denunciar de forma «preventiva» y sin pruebas concretas.                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </main>
     )
 }
+
+export default Denuncias;
