6a3dbbc7797073cdba9414fcfc17382d61eab36f
gevega <gevegaecheverria@gmail.com>
2025-02-09 14:44:39 -0300

    feat: Add Historias modal

--- src/app/(main)/historias/components/Card.jsx
+++ src/app/(main)/historias/components/Card.jsx
@@ -1,23 +1,61 @@
+"use client";
 import Image from "next/image";
-import Link from "next/link";
+import { useState } from "react";
 
-function Card({ data, alt, url = '' }) {
+function Card({ name, data, alt, info }) {
+    const [modal, setModal] = useState(false);
     return (
         <div className="relative w-full h-auto mb-4 cursor-pointer">
-            <Link href={url} target="_blank" rel="noopener noreferrer">
-                <Image
-                    src={data}
-                    alt={alt}
-                    width={700}
-                    height={700}
-                    className="w-full h-auto"
-                    loading="eager"
-                />
+            <Image
+                src={data}
+                alt={alt}
+                width={700}
+                height={700}
+                className="w-full h-auto"
+                loading="eager"
+            />
 
-                <div className="absolute flex w-full h-full justify-center items-center bg-black/50 top-0 opacity-0 hover:opacity-100 duration-150">
-                    <h2 className="text-h3 text-white font-bold">CABALLITO</h2>
+            <div className="group absolute flex w-full h-full justify-center items-center bg-black/0 top-0 hover:bg-black/50 duration-150">
+                <button className="w-full h-full flex justify-center items-center" type="button" onClick={() => setModal(true)}>
+                    <h2 className="text-h3 text-white font-bold hidden group-hover:flex uppercase">{name}</h2>
+                </button>
+            </div>
+
+            {/* Modal */}
+            {modal && (
+                <div
+                    className="flex fixed inset-0 w-full h-full bg-black bg-opacity-50 backdrop-blur-sm z-50"
+                >
+                    <div className="flex fixed inset-0 mx-mobile sm:mx-tablet lg:mx-desktop justify-center items-center gap-8" role="dialog" aria-modal="true">
+                        <div className="w-full m-auto max-w-content max-h-[95%] overflow-hidden overflow-y-auto no-scrollbar grid grid-cols-1 md:grid-cols-[50%_1fr] lg:grid-cols-[30%_1fr] items-center gap-4 lg:gap-14 px-6 py-8 lg:px-16 bg-white rounded-3xl shadow-lg relative">
+                            <div className="flex w-[50%] md:w-full !aspect-square overflow-hidden relative">
+                                <Image
+                                    src={data}
+                                    alt={alt}
+                                    width={900}
+                                    height={900}
+                                    className="w-full h-full absolute object-cover"
+                                />
+                            </div>
+
+                            <div className="flex h-full flex-col gap-4">
+                                <h1 className="font-bold text-h2">{name}</h1>
+                                <p className="!font-bold par-1 text-primary">
+                                    {info}
+                                </p>
+                            </div>
+
+                            <button
+                                className="absolute top-2 right-4 heading-2 cursor-pointer hover:text-primaryColor duration-200 rounded-full bg-white shadow-gray-400 hover:bg-primary hover:text-white shadow-md w-8 h-8"
+                                onClick={() => setModal(false)}
+                                aria-label="Cerrar modal"
+                            >
+                                ✖
+                            </button>
+                        </div>
+                    </div>
                 </div>
-            </Link>
+            )}
         </div>
     )
 }
--- src/app/(main)/historias/page.jsx
+++ src/app/(main)/historias/page.jsx
@@ -3,24 +3,32 @@ import Card from "./components/Card";
 
 const items = [
     {
-        data: '/assets/caballos_atardecer.jpg',
+        id: '001',
+        name: 'Magnolia',
+        data: '/assets/historias/historias_001.jpg',
         alt: '',
-        url: 'https://www.youtube.com/',
+        info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer gravida neque et enim feugiat, ut interdum nulla lacinia. Vivamus cursus eros non lectus efficitur, a tincidunt ante tempor. Ut sagittis urna non ligula consectetur, vel gravida purus dictum. Nam nec arcu ac turpis laoreet sollicitudin id at purus. Donec ullamcorper, ligula vel dapibus dignissim, lorem est laoreet justo, vitae accumsan ante nulla at odio. Phasellus sollicitudin viverra leo, a tempus metus vulputate nec. Pellentesque in fringilla ligula. Morbi et feugiat velit, nec mollis arcu. Aliquam erat volutpat. Nam viverra bibendum purus sit amet tincidunt. Sed sed justo ligula. Etiam id dictum lorem. Integer tincidunt ipsum ut leo fermentum malesuada. Integer tincidunt tincidunt nunc, a eleifend neque convallis ac. Morbi maximus ultricies orci, nec vulputate lectus. Curabitur efficitur velit ut augue rhoncus gravida. Fusce et velit sed tortor malesuada tristique non at augue. Nulla facilisi. Fusce lobortis dolor a libero auctor, ac maximus felis tempor. Suspendisse potenti. Vivamus euismod bibendum ante, non vehicula leo placerat in. Integer non felis non leo malesuada tincidunt ut at nulla. Integer ac leo nec nulla sollicitudin pretium. Sed mollis orci nec dolor tristique maximus. Sed et eros id felis laoreet auctor. Nunc convallis erat sit amet erat vulputate, non scelerisque ante bibendum. Sed gravida ipsum ac dolor tincidunt pretium.'
     },
     {
-        data: '/assets/caballos_rescatados.jpg',
+        id: '002',
+        name: 'Sofía',
+        data: '/assets/historias/historias_002.jpg',
         alt: '',
-        url: '',
+        info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
     },
     {
-        data: '/assets/doctor_veterinario.jpg',
+        id: '003',
+        name: 'Bella',
+        data: '/assets/historias/historias_003.jpg',
         alt: '',
-        url: '',
+        info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
     },
     {
-        data: '/assets/equipo_ong_caballos.jpg',
+        id: '004',
+        name: 'Pepe',
+        data: '/assets/historias/historias_004.jpg',
         alt: '',
-        url: '',
+        info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
     },
 ]
 
@@ -35,7 +43,7 @@ function Historias() {
 
                 <div className="masonry columns-2 gap-4">
                     {items.map((item) => (
-                        <Card data={item.data} alt={item.alt} url={item.url ? item.url : ''} />
+                        <Card key={item.id} name={item.name} data={item.data} alt={item.alt} info={item.info} />
                     ))}
                 </div>
 
