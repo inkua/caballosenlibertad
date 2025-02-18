4cd04b6006179d5951d15abbaeb92038c2b25b30
Paola Andrea Moreno <136043704+IngSw-Moreno7@users.noreply.github.com>
2025-02-18 14:08:19 -0500

    Creating error.js and not-found.jsx files in the application folder and in the assets folder creating the errors folder where the error images are (#71)

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,27 +1,45 @@
-function NotFound() {
-  return (
-    <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8 h-[100vh]">
-      <div className="text-center">
-        <p className="text-base font-semibold text-indigo-600">404</p>
-        <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-          Page not found
-        </h1>
-        <p className="mt-6 text-base leading-7 text-gray-600">
-          Sorry, we couldn't find the page you're looking for.
-        </p>
-        <div className="mt-10 flex items-center justify-center gap-x-6">
-          <a
-            href="/"
-            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-            Go back home
-          </a>
-          <a href="#" className="text-sm font-semibold text-gray-900">
-            Contact support <span aria-hidden="true">&rarr;</span>
-          </a>
-        </div>
-      </div>
-    </main>
-  );
-}
+"use client";
 
-export default NotFound;
+export default function NotFoundPage() {
+  return (
+        <body>
+            <section className="bg-[#F2F6F8] min-h-screen flex items-center">
+              <div className="container px-6 py-12 mx-auto lg:flex lg:items-center lg:gap-12">
+                <div className="w-full lg:w-1/2">
+                  <p className="text-base font-normal text-[#00638C]">Error 404</p>
+                  <h1 className="mt-3 text-4xl font-semibold text-[#00638C] md:text-5xl">
+                    Página no encontrada
+                  </h1>
+                  <p className="mt-4 text-xl text-[#03151F]">
+                    La página que buscas no existe...
+                  </p>
+        
+                  {/* Botones */}
+                  <div className="flex justify-start items-center mt-6 gap-x-3">
+                    <button onClick={() => window.history.back()}  
+                      className="w-44 px-5 py-2 text-sm font-bold text-[#00638C] transition-colors duration-200 bg-transparent border border-[#00638C66] rounded-full hover:bg-[#00638C] hover:text-white"
+                    > <span>ATRÁS</span> </button>
+        
+                    <a 
+                      href="/" 
+                      className="w-auto px-5 py-2 text-sm font-bold tracking-widest text-[#F2F6F8] duration-200 bg-[#D98729] border border-[#F2F6F8] rounded-full hover:bg-transparent hover:text-[#D98729] hover:border-[#D98729]"
+                    >
+                      VOLVER AL INICIO
+                    </a>
+                  </div>
+                </div>
+        
+                {/* Imagen */}
+                <div className="relative w-full mt-8 lg:w-1/2 lg:mt-0">
+                  <img 
+                    className="w-full h-80 md:h-96 rounded-lg object-cover" 
+                    src="/assets/errors/tarde_caballos.jpg" 
+                    alt="Page not found"
+                  />
+                </div>
+                
+              </div>
+            </section>
+        </body>
+          );
+        }
