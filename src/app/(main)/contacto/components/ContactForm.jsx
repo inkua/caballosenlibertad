88d2f202a97297e64ca2c412140423823bdf1e14
gevega <gevegaecheverria@gmail.com>
2025-02-23 12:05:58 -0300

    feat: Add form validate to Contact with react-hook-form and yup

--- src/app/(main)/contacto/components/ContactForm.jsx
+++ src/app/(main)/contacto/components/ContactForm.jsx
@@ -2,56 +2,37 @@
 
 import { useToast } from "@/utils/toast";
 import { useState } from "react";
+import { useForm } from "react-hook-form"
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
+
+const schema = yup
+    .object({
+        name: yup.string().required('Debe ingresar un nombre válido'),
+        phone: yup.number()
+            .positive('Ingrese un numero positivo')
+            .integer('Ingrese un numero entero')
+            .required('Debe ingresar un numero válido')
+            .transform((value) => Number.isNaN(value) ? null : value),
+        email: yup.string().email('Debe ingresar un email válido').required('Debe ingresar un email válido'),
+        text: yup.string().required('Debe ingresar una consulta'),
+    })
+    .required()
 
 export default function ContactForm() {
-    const [formData, setFormData] = useState({
-        name: "",
-        phone: "",
-        email: "",
-        message: "",
-    });
-    const [error, setError] = useState({
-        name: "",
-        phone: "",
-        email: "",
-        message: ""
-    });
+    const {
+        register,
+        reset,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({
+        resolver: yupResolver(schema),
+    })
+
     const { showToast } = useToast()
     const [pending, setPending] = useState(false);
 
-    const handleChange = (e) => {
-        setFormData({ ...formData, [e.target.name]: e.target.value });
-    };
-
-    const isValidEmail = (email) => {
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        return emailRegex.test(email);
-    };
-
-    const isValidPhone = (phone) => {
-        const phoneRegex = /^\d{7,15}$/; // Valida si es un número de teléfono con entre 7 y 15 dígitos
-        return phoneRegex.test(phone);
-    };
-
-    const sendEmail = async (e) => {
-        e.preventDefault();
-        const newErrors = {}
-        const { name, phone, email, message } = formData;
-        if (name.trim().length < 3) {
-            newErrors.name = "El nombre debe tener minimo 3 caracteres"
-        } else if (!isValidPhone(phone)) {
-            newErrors.email = "Email invalido"
-        } else if (!isValidEmail(email)) {
-            newErrors.phone = "Teléfono invalido"
-        } else if (message.trim().length < 10) {
-            newErrors.message = "La consulta debe tener minimo 10 caracteres"
-        }
-
-        if (Object.keys(newErrors).length > 0) {
-            setError(newErrors)
-            return
-        }
-        setError({})
+    const onSubmit = async (formData) => {
         setPending(true)
 
         try {
@@ -59,7 +40,7 @@ export default function ContactForm() {
                 name: formData.name,
                 phone: formData.phone,
                 email: formData.email,
-                content: formData.message,
+                content: formData.text,
             }
 
             const response = await fetch('/api/mail', {
@@ -69,8 +50,8 @@ export default function ContactForm() {
             })
 
             if (response.ok) {
-                setFormData({ name: '', phone: '', email: '', message: '' }); 
                 showToast({ type: 'success', message: '¡Tu consulta ha sido enviada con éxito!' })
+                reset()
             } else {
                 showToast({ type: 'error', message: 'No pudimos enviar tu consulta. Inténtalo nuevamente.' })
             }
@@ -90,71 +71,68 @@ export default function ContactForm() {
             <h2 htmlFor="name" className="block text-primary text-p3">
                 Nombre
             </h2>
-            <form className="flex flex-col gap-4 h-auto" onSubmit={(e) => sendEmail(e)}>
+            <form className="flex flex-col gap-4 h-auto" onSubmit={handleSubmit(onSubmit)}>
                 <div className="flex flex-col w-full h-full lg:aspect-square gap-4">
-                    <div className="flex flex-col gap-4">
+                    <div className="relative flex flex-col gap-4">
                         <label htmlFor="name" className="text-primary text-p3 hidden">
                             Nombre
                         </label>
                         <input
-                            required
                             type="text"
                             id="name"
                             name="name"
                             placeholder="Nombre y apellido"
+                            maxLength={30}
                             className="w-full h-12 rounded-md border-2 text-primary border-input placeholder-input"
-                            onChange={handleChange}
-                            value={formData.name}
+                            onInput={(e) => { e.target.value = e.target.value.replace(/[0-9]/g, '') }}
+                            {...register("name")}
                         />
-                        {error.name && <span className="text-red-500">{error.name}</span>}
+                        {errors.name && <span className="absolute right-0 bottom-[-1.5rem] text-red-500">{errors.name.message}</span>}
                     </div>
-                    <div className="flex flex-col gap-4">
+                    <div className="relative flex flex-col gap-4">
                         <label htmlFor="phone" className="block text-primary text-p3">
                             Número de teléfono
                         </label>
                         <input
-                            required
                             type="text"
                             id="phone"
                             name="phone"
                             placeholder="Teléfono"
+                            maxLength={15}
                             className="w-full h-12 rounded-md border-2 text-primary border-input placeholder-input appearance-none"
-                            onChange={handleChange}
-                            onInput={(e) => (e.target.value = e.target.value.replace(/\D/g, ""))}
-                            value={formData.phone}
+                            onInput={(e) => { e.target.value = e.target.value.replace(/[^0-9+\s\-()]/g, '') }}
+                            {...register("phone")}
                         />
-                        {error.phone && <span className="text-red-500">{error.phone}</span>}
+                        {errors.phone && <span className="absolute right-0 bottom-[-1.5rem] text-red-500">{errors.phone.message}</span>}
                     </div>
-                    <div className="flex flex-col gap-4">
+                    <div className="relative flex flex-col gap-4">
                         <label htmlFor="email" className="block text-primary text-p3">
                             Correo electrónico
                         </label>
                         <input
-                            required
                             type="text"
                             id="email"
                             name="email"
                             placeholder="Correo electrónico"
+                            maxLength={30}
                             className="w-full h-12 rounded-md border-2 text-primary border-input placeholder-input"
-                            onChange={handleChange}
-                            value={formData.email}
+                            {...register("email")}
                         />
-                        {error.email && <span className="text-red-500">{error.email}</span>}
+                        {errors.email && <span className="absolute right-0 bottom-[-1.5rem] text-red-500">{errors.email.message}</span>}
                     </div>
-                    <div className="flex flex-col gap-4 flex-1">
+                    <div className="relative flex flex-col gap-4 flex-1">
                         <label htmlFor="message" className="block text-primary text-p3">
                             Déjanos tu consulta
                         </label>
                         <textarea
-                            required
                             placeholder="Consulta..."
                             id="message"
                             name="message"
+                            maxLength={500}
                             className="w-full md:flex-1 h-36 md:h-auto rounded-md border-2 text-primary border-input placeholder-input resize-none"
-                            onChange={handleChange}
-                            value={formData.message}
-                        ></textarea>
-                        {error.message && <span className="text-red-500">{error.message}</span>}
+                            {...register("text")}
+                        />
+                        {errors.text && <span className="absolute right-0 bottom-[-1.5rem] text-red-500">{errors.text.message}</span>}
                     </div>
                 </div>
 
