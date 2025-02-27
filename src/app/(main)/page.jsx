c2f5a45ea4eac758fd2c45baf3b1ad62ab6ac58d
gevega <gevegaecheverria@gmail.com>
2025-02-27 11:44:35 -0300

    feat: Semantic HTML Home page & layout

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -8,20 +8,22 @@ import VideoThumbnail from "./components/home/VideoThumbnail";
 export default function Home() {
     return (
         <main className='pt-[70px] lg:pt-[90px] '>
-            <TitleSection
-                titleTextContent='Caballos en Libertad'
-                spanTextContent='Protección, cuidado y rescate de equinos'
-            >
-                <Link href={'/nosotros'} className='button-primary font-semibold md:text-xl mt-4 lg:mt-8 md:w-[360px] md:h-[64px] col-start-1 w-fit'>
-                    CONOCENOS
-                </Link>
-            </TitleSection>
+            <section id="hero" role="banner">
+                <TitleSection
+                    titleTextContent='Caballos en Libertad'
+                    spanTextContent='Protección, cuidado y rescate de equinos'
+                >
+                    <Link href={'/nosotros'} className='button-primary font-semibold md:text-xl mt-4 lg:mt-8 md:w-[360px] md:h-[64px] col-start-1 w-fit'>
+                        CONOCENOS
+                    </Link>
+                </TitleSection>
+            </section>
 
-            <section className="w-full mt-4 max-w-banner mx-auto">
+            <section role="region" className="w-full mt-4 max-w-banner mx-auto">
                 <VideoThumbnail />
             </section>
 
-            <section className='flex flex-col w-full box-border items-start justify-between gap-20 lg:gap-40 py-20 md:py-24 lg:py-32 xl:py-40 px-mobile md:px-tablet lg:px-desktop max-w-content mx-auto'>
+            <section role="region" className='flex flex-col w-full box-border items-start justify-between gap-20 lg:gap-40 py-20 md:py-24 lg:py-32 xl:py-40 px-mobile md:px-tablet lg:px-desktop max-w-content mx-auto'>
                 <Statistics />
                 <Contribution />
             </section>
