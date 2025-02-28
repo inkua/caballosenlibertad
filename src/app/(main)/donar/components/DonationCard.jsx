3650302c539b53890322d451bf4e1310e471ceb4
manuel14mds <manuel14mds@gmail.com>
2025-02-28 18:52:31 -0500

    update(contributions page): conexion persistence

--- src/app/(main)/donar/components/DonationCard.jsx
+++ src/app/(main)/donar/components/DonationCard.jsx
@@ -1,12 +1,61 @@
-'use client'
+import { getUrlBase } from '@/utils/urlRoute';
 import { SelectForm } from './SelectForm'
 
-export const DonationCard = () => {
+const info = {
+    paypal: {
+        link: "https://www.instagram.com/caballosenlibertadlp"
+    },
+    mercadoPago: {
+        monthlySubscription: "https://www.instagram.com/caballosenlibertadlp",
 
-  return (
-    <div className='flex flex-col items-center justify-start bg-white p-8 py-10 sm:p-12 xl:px-14 rounded-2xl sm:rounded-[20px] gap-6 w-full lg:min-w-[420px] max-w-[480px] lg:max-w-[520px]'>
-      <h3 className='text-primary text-[28px] lg:text-[35px] font-bold text-start w-full'>¡Haz tu aporte!</h3>
-      <SelectForm />
-    </div>
-  )
+        one_timePayment: {
+
+            amountTwo: {
+                link: "https://www.instagram.com/caballosenlibertadlp",
+                quantity: "200"
+            },
+            amountThree: {
+                quantity: "300",
+                link: "https://www.instagram.com/caballosenlibertadlp"
+            },
+            otherAmount: {
+                quantity: "otro",
+                link: "https://www.instagram.com/caballosenlibertadlp"
+            },
+            amountOne: {
+                link: "https://www.instagram.com/caballosenlibertadlp",
+                quantity: "100"
+            }
+
+        }
+    }
+}
+
+const getData = async () => {
+    const urlBase = getUrlBase()
+    const url = `${urlBase}/api/payments`
+    try {
+        const response = await fetch(url, {
+            next: {
+                revalidate: 60
+            }
+        });
+        const result = await response.json();
+        if(result.status==200){
+            return result.data
+        }
+        return info
+    } catch (error) {
+        return info
+    }
+}
+
+export const DonationCard = async () => {
+    const payments = await getData()
+    return (
+        <div className='flex flex-col items-center justify-start bg-white p-8 py-10 sm:p-12 xl:px-14 rounded-2xl sm:rounded-[20px] gap-6 w-full lg:min-w-[420px] max-w-[480px] lg:max-w-[520px]'>
+            <h2 className='text-primary text-[28px] lg:text-[35px] font-bold text-start w-full'>¡Haz tu aporte!</h2>
+            <SelectForm data={payments} />
+        </div>
+    )
 }
--- src/app/(main)/donar/contribuciones/page.jsx
+++ src/app/(main)/donar/contribuciones/page.jsx
@@ -9,16 +9,16 @@ function Contribuciones() {
 
   return (
     <main className="pt-[70px] lg:pt-[90px]">
-      <div className='w-full mx-auto pb-4 md:py-6 lg:pt-8 lg:pb-12 px-mobile md:px-tablet lg:px-desktop max-w-content'>
+      <section className='w-full mx-auto pb-4 md:py-6 lg:pt-8 lg:pb-12 px-mobile md:px-tablet lg:px-desktop max-w-content'>
         <h1
           className="text-[28px] sm:text-[32px] md:text-[32px] lg:text-[44px] xl:text-[50px] leading-140 font-extrabold text-primary mb-4 lg:mb-10 pt-6 md:pt-12 lg:pt-14"
         >Elije si quieres donar mensualmente o por única vez</h1>
-      </div>
-      <div className="relative flex sm:min-h-[1046px] w-full sm:bg-[url('/assets/donar/IMG_back_formulario-de-pago.png')] sm:bg-cover sm:bg-center max-w-banner">
+      </section>
+      <section className="relative flex sm:min-h-[1046px] w-full sm:bg-[url('/assets/donar/backimage.webp')] sm:bg-cover sm:bg-center max-w-banner">
         <div className="w-full my-auto flex items-center justify-center lg:justify-end lg:mr-[9%]">
           <DonationCard />
         </div>
-      </div>
+      </section>
       <BankAccountCard />
     </main>
   );
